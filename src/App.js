46075a5079aee6623e09c48bee0de48d98f5bce0
legoffchristophealexandre@gmail.com <legoffchristophealexandre@gmail.com>
2024-07-24 09:08:56 +0200

    Add Loader

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Blogs from './pages/Blogs';
 import NoPage from './pages/NoPage.jsx';
+import './styles/reset.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index element={<Home color={'white'} />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="*" element={<NoPage />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,5 @@
-import { useEffect } from 'react';
-
 const Home = () => {
-  useEffect(() => {
-    fetch('https://localhost:8000/api/tasks')
-      .then((res) => res.json())
-      .then((data) => {
-        return data['hydra:member'];
-      });
-  }, [tasks]);
-
-  console.log(tasks);
-  return (
-    <div>
-      <span></span>
-    </div>
-  );
+  return <h1>Accueil</h1>;
 };
 
 export default Home;
